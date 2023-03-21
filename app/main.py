58c659eb946b23ebda9c90faf34a86cad31866b5
Guseyn Salimov <maximweller@protonmail.com>
2023-03-21 11:56:34 +0300

    feat: mvp

--- app/main.py
+++ app/main.py
@@ -1,1 +1,19 @@
-print("hello world")
+from flask import Flask, render_template
+from fields import *
+
+
+app = Flask(__name__)
+app.secret_key = "hey"
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    reg_form = RegistrationForm()
+    if reg_form.validate_on_submit():
+        return "Great success"
+
+    return render_template("index.html", form=reg_form)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
